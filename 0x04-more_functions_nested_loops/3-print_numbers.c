84c460cf72baef808ef9d784f64a60387b934523
estherfagbemi <fagbemiesteri203@gmail.com>
2023-10-12 15:30:46 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
